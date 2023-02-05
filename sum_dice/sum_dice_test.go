e6920a27157998066c6467e08cbfcc96a472961a
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-02-05 19:53:32 +0300

    Add tests

--- sum_dice/sum_dice_test.go
+++ sum_dice/sum_dice_test.go
@@ -1,69 +1,33 @@
 package sum_dice_test
 
 import (
-	"math"
 	"strconv"
 	"strings"
 	"testing"
 
 	. "github.com/DmitryBogomolov/dicecalc/probabilities"
 	. "github.com/DmitryBogomolov/dicecalc/sum_dice"
+	"github.com/DmitryBogomolov/dicecalc/test_helper"
 )
 
-func TestX(t *testing.T) {
+func Test3dX(t *testing.T) {
+	checkProbabilities(t, "3d4")
 	checkProbabilities(t, "3d6")
+	checkProbabilities(t, "3d10")
+	checkProbabilities(t, "3d20")
+}
+
+func Test4dX(t *testing.T) {
+	checkProbabilities(t, "4d4")
 	checkProbabilities(t, "4d6")
-	checkProbabilities(t, "5d6")
-	checkProbabilities(t, "5d5")
-	checkProbabilities(t, "5d6")
+	checkProbabilities(t, "4d10")
+	checkProbabilities(t, "4d16")
+	checkProbabilities(t, "4d20")
 }
 
-func measureRoll(dices []int) int {
-	sum := 0
-	for _, dice := range dices {
-		sum += dice
-	}
-	return sum
-}
-
-func advanceRoll(dices []int, diceSides int) {
-	for idx := len(dices) - 1; idx >= 0; idx-- {
-		dices[idx]++
-		if dices[idx] > diceSides {
-			dices[idx] = 1
-		} else {
-			break
-		}
-	}
-}
-
-func makeExpectedData(diceCount, diceSides int) []float64 {
-	dices := make([]int, diceCount)
-	total := 1
-	for i := range dices {
-		dices[i] = 1
-		total *= diceSides
-	}
-	counter := make([]int, diceCount*diceSides-diceCount+1)
-	for i := 0; i < total; i++ {
-		value := measureRoll(dices)
-		advanceRoll(dices, diceSides)
-		counter[value-diceCount]++
-	}
-	data := make([]float64, len(counter))
-	for i := range counter {
-		data[i] = float64(counter[i]) / float64(total)
-	}
-	return data
-}
-
-func makeActualData(diceCount, diceSides int) []float64 {
-	probabilies, _ := CalculateProbabilities(DiceRollParameters{DiceCount: diceCount, DiceSides: diceSides})
-	data := make([]float64, probabilies.ValuesCount())
-	for i := range data {
-		data[i] = probabilies.ValueProbability(probabilies.MinValue() + i)
-	}
-	return data
+func Test10dX(t *testing.T) {
+	checkProbabilities(t, "10d1")
+	checkProbabilities(t, "10d6")
 }
 
 func checkProbabilities(t *testing.T, name string) {
@@ -71,12 +35,16 @@ func checkProbabilities(t *testing.T, name string) {
 		parts := strings.Split(name, "d")
 		count, _ := strconv.Atoi(parts[0])
 		sides, _ := strconv.Atoi(parts[1])
-		expected := makeExpectedData(count, sides)
-		actual := makeActualData(count, sides)
-		for i := range expected {
-			if math.Abs(actual[i]-expected[i]) > 0.0001 {
-				t.Errorf("%dth element: expected %f actual %f", i, expected[i], actual[i])
+		params := DiceRollParameters{DiceCount: count, DiceSides: sides}
+		measure := func(roll []int) int {
+			sum := 0
+			for _, dice := range roll {
+				sum += dice
 			}
+			return sum
 		}
+		expected := test_helper.CollectTestData(params, measure)
+		actual, _ := CalculateProbabilities(params)
+		test_helper.CheckProbabilities(t, expected, actual)
 	})
 }
