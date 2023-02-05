7888236838d44c1fa829d43371e3204b62563930
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-02-05 14:34:00 +0300

    Update helper

--- test.py
+++ test.py
@@ -1,30 +1,51 @@
 #/usr/bin/env python3
 
 from typing import Final, List, Dict
+import math
+
+DiceRoll = List[int]
 
 DICE_COUNT: Final[int] = 3
 DICE_SIDES: Final[int] = 6
 
 total_count = DICE_SIDES ** DICE_COUNT
 
-def init_roll() -> List[int]:
+def init_roll() -> DiceRoll:
     return [1 for _ in range(DICE_COUNT)]
 
-def advance_roll(roll: List[int]) -> None:
-    for idx in range(len(roll) - 1, -1, -1):
+def advance_roll(roll: DiceRoll) -> None:
+    for idx in range(DICE_COUNT - 1, -1, -1):
         roll[idx] = roll[idx] + 1
         if roll[idx] > DICE_SIDES:
             roll[idx] = 1
         else:
             break
 
-def measure_roll(roll: List[int]) -> int:
+def measure_roll(roll: DiceRoll) -> int:
     return sum(roll)
 
-def get_roll_key(roll: List[int]) -> str:
+def get_roll_key(roll: DiceRoll) -> str:
     return ''.join(map(str, sorted(roll)))
 
-def main() -> None:
+def get_roll_idx(roll: DiceRoll) -> int:
+    idx = 0
+    k = 1
+    for i in range(DICE_COUNT - 1, -1, -1):
+        idx += k * (roll[i] - 1)
+        k *= DICE_SIDES
+    return idx
+
+def get_roll_from_idx(idx: int) -> DiceRoll:
+    roll = init_roll()
+    residue = idx
+    factor = total_count
+    for i in range(DICE_COUNT):
+        factor //= DICE_SIDES
+        k, residue = divmod(residue, factor)
+        roll[i] = k + 1
+    return roll
+
+def test_1() -> None:
     roll = init_roll()
     index: Dict[int, Dict[str, int]] = {}
     for _ in range(total_count):
@@ -48,5 +69,27 @@ def main() -> None:
     if check_sum != total_count:
         raise RuntimeError('mismatch')
 
+def test_2() -> None:
+    roll = init_roll()
+    index: Dict[int, List[int]] = {}
+    for _ in range(total_count):
+        idx = get_roll_idx(roll)
+        val = measure_roll(roll)
+        index_item = index.get(val)
+        if not index_item:
+            index_item = []
+            index[val] = index_item
+        index_item.append(idx)
+        advance_roll(roll)
+
+    width = math.ceil(math.log10(total_count))
+    for val in index.keys():
+        index_item = index[val]
+        data = ' '.join(map(lambda t: str(t).rjust(width), index_item))
+        diffs = [index_item[i] - index_item[i - 1] for i in range(1, len(index_item))]
+        diffs_data = ' '.join(map(lambda t: str(t).rjust(width - 1), diffs))
+        print(f'{val:2}: {data}')
+        print(f'  {diffs_data}')
+
 if __name__ == '__main__':
-    main()
+    test_2()
