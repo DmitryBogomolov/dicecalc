49581e3cd3c52a65a59e5874700bd6e058a6d579
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-03-08 14:22:47 +0300

    Update test

--- sum_dice/sum_dice_test.go
+++ sum_dice/sum_dice_test.go
@@ -11,5 +11,7 @@ import (
 func TestCalculateProbabilities(t *testing.T) {
 	probs, _ := CalculateProbabilities(dice_roller.DiceRollParameters{DiceCount: 2, DiceSides: 4})
 	assert.Equal(t, 2, probs.MinValue())
-	assert.Equal(t, 12, probs.MaxValue())
+	assert.Equal(t, 8, probs.MaxValue())
+	assert.Equal(t, uint64(16), probs.TotalCount())
+	assert.Equal(t, 7, probs.ValuesCount())
 }
