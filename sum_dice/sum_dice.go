4d1837683521f075b3bb0c036aebb8faa8c7f1ee
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-03-08 19:16:43 +0300

    Add description

--- sum_dice/sum_dice.go
+++ sum_dice/sum_dice.go
@@ -6,12 +6,23 @@ import (
 	"github.com/DmitryBogomolov/dicecalc/dice_roller"
 )
 
+// CalculateProbabilities returns probabilities of sums for dice rolls.
+//
+// For example for 2d6 rolls probability of 2 (1+1) and 12 (6+6) is 1/36,
+// probability of 3 (1+2, 2+1) and 11 (5+6, 6+5) is 2/36, etc.
+//
+// For rolls of n m-sided dices consider n-dimensional cube with side of m.
+// That cube consists of smaller m^n cubes with side of 1. Each small cube defines one specific roll.
+//
+// Plane x_1 + ... + x_n = t (0 < t < n * m) intersects set of small cubes.
+// All of them define rolls with the same sum. Numbers of cubes gives probability of that sum.
 func CalculateProbabilities(params dice_roller.DiceRollParameters) (*dice_roller.Probabilities, error) {
 	minVal := params.DiceCount
 	maxVal := params.DiceCount * params.DiceSides
 	count := maxVal - minVal + 1
 	variants := make([]int, count)
 	halfCount := int(math.Ceil(0.5 * float64(count)))
+	// Only half of cube is required to be inspected. The other half is symmetrical.
 	fillVariants(variants, halfCount, params.DiceCount, params.DiceSides)
 	fillSymmetricVariants(variants, halfCount)
 	total := uint64(math.Pow(float64(params.DiceSides), float64(params.DiceCount)))
