4ac079059f8cbc4897a751aa0acacef75820e223
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-03-08 15:11:32 +0300

    Add more tests

--- sum_dice/sum_dice_test.go
+++ sum_dice/sum_dice_test.go
@@ -1,11 +1,14 @@
 package sum_dice
 
 import (
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/DmitryBogomolov/dicecalc/dice_roller"
+	"github.com/DmitryBogomolov/dicecalc/test_helper"
 )
 
 func TestCalculateProbabilities(t *testing.T) {
@@ -14,4 +17,43 @@ func TestCalculateProbabilities(t *testing.T) {
 	assert.Equal(t, 8, probs.MaxValue())
 	assert.Equal(t, uint64(16), probs.TotalCount())
 	assert.Equal(t, 7, probs.ValuesCount())
+}
+
+func Test3dX(t *testing.T) {
+	checkProbabilities(t, "3d4")
+	checkProbabilities(t, "3d6")
+	checkProbabilities(t, "3d10")
+	checkProbabilities(t, "3d20")
+}
+
+func Test4dX(t *testing.T) {
+	checkProbabilities(t, "4d4")
+	checkProbabilities(t, "4d6")
+	checkProbabilities(t, "4d10")
+	checkProbabilities(t, "4d16")
+	checkProbabilities(t, "4d20")
+}
+
+func Test10dX(t *testing.T) {
+	checkProbabilities(t, "10d1")
+	checkProbabilities(t, "10d6")
+}
+
+func checkProbabilities(t *testing.T, name string) {
+	t.Run(name, func(t *testing.T) {
+		parts := strings.Split(name, "d")
+		count, _ := strconv.Atoi(parts[0])
+		sides, _ := strconv.Atoi(parts[1])
+		params := dice_roller.DiceRollParameters{DiceCount: count, DiceSides: sides}
+		measure := func(roll []int) int {
+			sum := 0
+			for _, dice := range roll {
+				sum += dice
+			}
+			return sum
+		}
+		expected := test_helper.CollectTestData(params, measure)
+		actual, _ := CalculateProbabilities(params)
+		test_helper.CheckProbabilities(t, expected, actual)
+	})
 }
