916a71f055f3f6d1ed9f4d23a7b97a1cc77a601c
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-03-09 12:29:03 +0300

    Add app

---
+++ app/app.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/DmitryBogomolov/dicecalc/minmax_dice"
+	"github.com/DmitryBogomolov/dicecalc/probabilities"
+	"github.com/DmitryBogomolov/dicecalc/sum_dice"
+)
+
+type _Func func(probabilities.DiceRollParameters) (probabilities.Probabilities, error)
+
+var modes = map[string]_Func{
+	"sum": sum_dice.CalculateProbabilities,
+	"min": minmax_dice.CalculateMaxProbabilities,
+	"max": minmax_dice.CalculateMinProbabilities,
+}
+
+func main() {
+	modeVar := flag.String("mode", "", "operation")
+	schemaVar := flag.String("schema", "", "roll schema")
+	flag.Parse()
+	if len(os.Args) < 2 {
+		flag.Usage()
+		return
+	}
+
+	var fn _Func
+	var params probabilities.DiceRollParameters
+	var err error
+	if fn, err = parseMode(*modeVar); err != nil {
+		fmt.Println(err)
+		return
+	}
+	if params, err = parseRollSchema(*schemaVar); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var probs probabilities.Probabilities
+	if probs, err = fn(params); err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(probs)
+}
+
+func parseRollSchema(schema string) (params probabilities.DiceRollParameters, err error) {
+	items := strings.Split(strings.ToLower(schema), "d")
+	if len(items) != 2 {
+		err = fmt.Errorf("bad schema: %s", schema)
+		return
+	}
+	var diceCount int
+	if diceCount, err = strconv.Atoi(items[0]); err != nil {
+		err = fmt.Errorf("bad schema: %s", schema)
+		return
+	}
+	var diceSides int
+	if diceSides, err = strconv.Atoi(items[1]); err != nil {
+		err = fmt.Errorf("bad schema: %s", schema)
+		return
+	}
+	params.DiceCount = diceCount
+	params.DiceSides = diceSides
+	return
+}
+
+func parseMode(mode string) (_Func, error) {
+	if fn, has := modes[mode]; has {
+		return fn, nil
+	} else {
+		return nil, fmt.Errorf("bad mode: %s", mode)
+	}
+}
