43017e03b64dc12a545f9efb8bf6d4a988b18510
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-03-11 23:49:30 +0300

    Add some tests

---
+++ app/print_raw/print_test.go
@@ -0,0 +1,15 @@
+package print_raw_test
+
+import (
+	"testing"
+
+	"github.com/DmitryBogomolov/dicecalc/app/print_raw"
+	"github.com/DmitryBogomolov/dicecalc/probabilities"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPrint(t *testing.T) {
+	probs, _ := probabilities.NewProbabilities(2, 5, 7, []int{1, 2, 3, 1})
+	ret := print_raw.Print(probs, "Hello World")
+	assert.Equal(t, "Hello World\n 2   1  14.2857%\n 3   2  28.5714%\n 4   3  42.8571%\n 5   1  14.2857%\n", ret)
+}
