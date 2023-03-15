10b2eb9ed8d830bb1e158328827fb2470c76b1cd
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-03-15 10:48:55 +0300

    Update csv

--- wrapper/print_csv/print.go
+++ wrapper/print_csv/print.go
@@ -1,35 +1,21 @@
 package print_csv
 
 import (
-	"encoding/csv"
 	"fmt"
 	"strings"
 
 	"github.com/DmitryBogomolov/dicecalc/probabilities"
+	"github.com/DmitryBogomolov/dicecalc/wrapper/util"
 )
 
 func Print(probs probabilities.Probabilities, title string) []byte {
 	var builder strings.Builder
-	writer := csv.NewWriter(&builder)
-	writer.Write([]string{"value", "count", "probability"})
+	fmt.Fprintf(&builder, "# %s\n", title)
+	format := fmt.Sprintf("%%d,%%d,%%.%df%%%%\n", util.GetProbabilityPrecision(probs))
 	for i := 0; i < probs.Count(); i++ {
 		val, count, probability := probs.Item(i)
-		writer.Write([]string{fmt.Sprintf("%d", val), fmt.Sprintf("%d", count), fmt.Sprintf("%.2f%%", probability)})
+		fmt.Fprintf(&builder, format, val, count, probability*100)
 	}
-	writer.Flush()
-	// var obj _JsonObject
-	// obj.Title = title
-	// obj.Total = probs.TotalVariants()
-	// var items []_JsonItem
-	// for i := 0; i < probs.Count(); i++ {
-	// 	val, count, probability := probs.Item(i)
-	// 	var item _JsonItem
-	// 	item.Value = val
-	// 	item.Count = count
-	// 	item.Probability = probability
-	// 	items = append(items, item)
-	// }
-	// obj.Values = items
-	// ret, _ := json.MarshalIndent(obj, "", "  ")
+	fmt.Fprintf(&builder, "# Total count: %d\n", probs.TotalVariants())
 	return []byte(builder.String())
 }
