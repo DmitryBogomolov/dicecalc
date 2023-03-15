21e37e802d70c354259c2f90af60b101d2ed0179
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-03-16 00:37:42 +0300

    Update some more

--- server/server.go
+++ server/server.go
@@ -4,7 +4,6 @@ import (
 	_ "embed"
 	"fmt"
 	"html/template"
-	"io/ioutil"
 	"net/http"
 	"os"
 	"strconv"
@@ -13,6 +12,8 @@ import (
 )
 
 const PORT = 3001
+const DEFAULT_MODE = "sum"
+const DEFAULT_OUTPUT = "html"
 
 func main() {
 	mux := http.NewServeMux()
@@ -34,11 +35,15 @@ var tmplStr string
 var tmpl = template.Must(template.New("/").Parse(tmplStr))
 
 type _TemplateData struct {
-	Title     string
-	Modes     []string
-	Outputs   []string
-	DiceCount int
-	DiceSides int
+	Title   string
+	Modes   []string
+	Outputs []string
+	Init    struct {
+		DiceCount int
+		DiceSides int
+		Mode      int
+		Output    int
+	}
 }
 
 func handleRoot(writer http.ResponseWriter, req *http.Request) {
@@ -48,18 +53,32 @@ func handleRoot(writer http.ResponseWriter, req *http.Request) {
 	}
 
 	// TODO: Temporary for debug.
-	fileObj, _ := os.Open("./server/template.html")
-	defer fileObj.Close()
-	tmplStr_, _ := ioutil.ReadAll(fileObj)
-	tmpl_ := template.Must(template.New(".").Parse(string(tmplStr_)))
+	// fileObj, _ := os.Open("./server/template.html")
+	// defer fileObj.Close()
+	// tmplStr, _ := ioutil.ReadAll(fileObj)
+	// tmpl := template.Must(template.New(".").Parse(string(tmplStr)))
+
+	modes := wrapper.Modes()
+	outputs := wrapper.Outputs()
 
 	var data _TemplateData
-	data.Title = "Test"
-	data.Modes = wrapper.Modes()
-	data.Outputs = wrapper.Outputs()
-	data.DiceCount = 2
-	data.DiceSides = 6
-	tmpl_.Execute(writer, data)
+	data.Title = "Dice roll probabilities calculator"
+	data.Modes = modes
+	data.Outputs = outputs
+	data.Init.DiceCount = 2
+	data.Init.DiceSides = 6
+	data.Init.Mode = findIndex(modes, DEFAULT_MODE)
+	data.Init.Output = findIndex(outputs, DEFAULT_OUTPUT)
+	tmpl.Execute(writer, data)
+}
+
+func findIndex[T comparable](items []T, target T) int {
+	for i, item := range items {
+		if item == target {
+			return i
+		}
+	}
+	return -1
 }
 
 var contentTypes = map[string]string{
