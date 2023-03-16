bb66da5ac9fd94acee9bf3aef7ad9885747b27f1
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-03-16 13:32:16 +0300

    Add server shutdown

--- server/server.go
+++ server/server.go
@@ -1,26 +1,70 @@
 package main
 
 import (
-	_ "embed"
+	"context"
 	"fmt"
-	"html/template"
 	"net/http"
 	"os"
+	"os/signal"
 	"strconv"
-
-	"github.com/DmitryBogomolov/dicecalc/wrapper"
+	"syscall"
+	"time"
 )
 
-const PORT = 3001
-const DEFAULT_MODE = "sum"
-const DEFAULT_OUTPUT = "html"
+const (
+	PORT               = 3001
+	DEFAULT_DICE_COUNT = 2
+	DEFAULT_DICE_SIDES = 6
+	DEFAULT_MODE       = "sum"
+	DEFAULT_OUTPUT     = "html"
+	SHUTDOWN_TIMEOUT   = 5 * time.Second
+)
+
+var signals = []os.Signal{
+	os.Interrupt, os.Kill, syscall.SIGINT, syscall.SIGTERM,
+}
 
 func main() {
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", getPort()),
+		Handler: makeHandler(),
+	}
+	signalChannel := make(chan os.Signal)
+	signal.Notify(signalChannel, signals...)
+	serverChannel := make(chan error)
+	go runServer(server, serverChannel)
+	fmt.Printf("Running: %s\n", server.Addr)
+
+	select {
+	case err := <-serverChannel:
+		fmt.Printf("Error: %s\n", err)
+	case <-signalChannel:
+		fmt.Printf("Shutting down...\n")
+		if err := shutdownServer(server); err != nil {
+			fmt.Printf("Failed to shutdown: %s\n", err)
+		}
+	}
+}
+
+func makeHandler() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handleRoot)
 	mux.HandleFunc("/calculate", handleCalculate)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", getPort()), mux)
-	fmt.Println(err)
+	return mux
+}
+
+func runServer(server *http.Server, ch chan<- error) {
+	err := server.ListenAndServe()
+	if err == http.ErrServerClosed {
+		err = nil
+	}
+	ch <- err
+}
+
+func shutdownServer(server *http.Server) error {
+	ctx, cancel := context.WithTimeout(context.Background(), SHUTDOWN_TIMEOUT)
+	defer cancel()
+	return server.Shutdown(ctx)
 }
 
 func getPort() int {
@@ -28,81 +72,4 @@ func getPort() int {
 		return port
 	}
 	return PORT
-}
-
-//go:embed template.html
-var tmplStr string
-var tmpl = template.Must(template.New("/").Parse(tmplStr))
-
-type _TemplateData struct {
-	Title   string
-	Modes   []string
-	Outputs []string
-	Init    struct {
-		DiceCount int
-		DiceSides int
-		Mode      int
-		Output    int
-	}
-}
-
-func handleRoot(writer http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodGet {
-		http.Error(writer, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-
-	// TODO: Temporary for debug.
-	// fileObj, _ := os.Open("./server/template.html")
-	// defer fileObj.Close()
-	// tmplStr, _ := ioutil.ReadAll(fileObj)
-	// tmpl := template.Must(template.New(".").Parse(string(tmplStr)))
-
-	modes := wrapper.Modes()
-	outputs := wrapper.Outputs()
-
-	var data _TemplateData
-	data.Title = "Dice roll probabilities calculator"
-	data.Modes = modes
-	data.Outputs = outputs
-	data.Init.DiceCount = 2
-	data.Init.DiceSides = 6
-	data.Init.Mode = findIndex(modes, DEFAULT_MODE)
-	data.Init.Output = findIndex(outputs, DEFAULT_OUTPUT)
-	tmpl.Execute(writer, data)
-}
-
-func findIndex[T comparable](items []T, target T) int {
-	for i, item := range items {
-		if item == target {
-			return i
-		}
-	}
-	return -1
-}
-
-var contentTypes = map[string]string{
-	"raw":  "text/plain",
-	"html": "text/html",
-	"json": "application/json",
-	"svg":  "image/svg+xml",
-	"csv":  "text/plain",
-}
-
-func handleCalculate(writer http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodGet {
-		http.Error(writer, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	query := req.URL.Query()
-	mode := query.Get("mode")
-	output := query.Get("output")
-	schema := query.Get("schema")
-	if data, err := wrapper.Process(mode, schema, output); err != nil {
-		http.Error(writer, err.Error(), http.StatusBadRequest)
-	} else {
-		contentType := contentTypes[output]
-		writer.Header().Set("Content-Type", contentType)
-		writer.Write(data)
-	}
 }
