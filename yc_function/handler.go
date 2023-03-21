6ac9cd5a345d753e2502c07bc17d0e0da4720099
Dmitry Bogomolov <dmitry.bogomolov.mail@gmail.com>
2023-03-21 10:06:15 +0300

    Add calculation page response

--- yc_function/handler.go
+++ yc_function/handler.go
@@ -2,24 +2,21 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"strings"
 
-	"github.com/DmitryBogomolov/dicecalc/probabilities"
 	"github.com/DmitryBogomolov/dicecalc/server/pages"
-	"github.com/DmitryBogomolov/dicecalc/sum_dice"
 )
 
 type Request struct {
 	HttpMethod                      string              `json:"httpMethod"`
 	Headers                         map[string]string   `json:"headers"`
-	MultiValueHeaders               map[string][]string `json:"mutliValueHeaders"`
 	QueryStringParameters           map[string]string   `json:"queryStringParameters"`
 	MultiValueQueryStringParameters map[string][]string `json:"multiValueQueryStringParameters"`
 	RequestContext                  map[string]any      `json:"requestContent"`
 	Body                            string              `json:"body"`
 	IsBase64Encoded                 bool                `json:"isBase64Encoded"`
+	MultiValueHeaders               map[string][]string `json:"mutliValueHeaders"`
 }
 
 type Response struct {
@@ -30,37 +27,37 @@ type Response struct {
 	IsBase64Encoded   bool                `json:"isBase64Encoded"`
 }
 
-const calculationQuery = `.?mode=${mode}&output=${output}&schema=${diceCount}d${diseSides}`
+const calculationQuery = `?mode=${mode}&output=${output}&schema=${diceCount}d${diseSides}`
 
-func Handle(ctx context.Context, data []byte) ([]byte, error) {
-	fmt.Printf("name    : %s\n", ctx.Value("lambdaRuntimeFunctionName").(string))
-	fmt.Printf("version : %s\n", ctx.Value("lambdaRuntimeFunctionVersion").(string))
-	fmt.Printf("limit   : %dMb\n", ctx.Value("lambdaRuntimeMemoryLimit").(int))
-	fmt.Printf("request : %s\n", ctx.Value("lambdaRuntimeRequestID").(string))
-	fmt.Printf("byte: %v\n", data)
-	fmt.Printf("str : %v\n", string(data))
+func Handle(ctx context.Context, req *Request) (*Response, error) {
+	// fmt.Printf("name    : %s\n", ctx.Value("lambdaRuntimeFunctionName").(string))
+	// fmt.Printf("version : %s\n", ctx.Value("lambdaRuntimeFunctionVersion").(string))
+	// fmt.Printf("limit   : %dMb\n", ctx.Value("lambdaRuntimeMemoryLimit").(int))
+	// fmt.Printf("request : %s\n", ctx.Value("lambdaRuntimeRequestID").(string))
 
-	var req Request
-	isHttp := json.Unmarshal(data, &req) == nil
+	schema := req.QueryStringParameters["schema"]
+	mode := req.QueryStringParameters["mode"]
+	output := req.QueryStringParameters["output"]
 
-	probs, _ := sum_dice.CalculateProbabilities(probabilities.DiceRollParameters{
-		DiceCount: 2,
-		DiceSides: 6,
-	})
-
-	message := fmt.Sprintf("Probs: %d\nHello World\n", probs.TotalVariants())
 	var sb strings.Builder
-	pages.RenderSelection(&sb, calculationQuery)
+	var err error
+	if schema != "" || mode != "" || output != "" {
+		err = pages.RenderCalculation(&sb, schema, mode, output)
+	} else {
+		funcName := ctx.Value("lambdaRuntimeFunctionName").(string)
+		err = pages.RenderSelection(&sb, fmt.Sprintf("/%s%s", funcName, calculationQuery))
+	}
 
-	if isHttp {
-		var res Response
+	var res Response
+	if err != nil {
+		res.StatusCode = 500
+		res.Body = err.Error()
+	} else {
 		res.StatusCode = 200
-		res.Body = sb.String()
 		res.Headers = map[string]string{
 			"content-type": "text/html",
 		}
-		return json.Marshal(res)
+		res.Body = sb.String()
 	}
-
-	return []byte(message), nil
+	return &res, nil
 }
